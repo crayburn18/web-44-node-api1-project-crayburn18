5e85036747e4ab6281017dfb3461c652fff5b4c8
Connor Rayburn <connorrayburn.work@gmail.com>
2021-08-02 16:15:06 -0700

    completed all but two tests sucessfully

--- api/server.js
+++ api/server.js
@@ -1,3 +1,89 @@
+const express = require('express')
+const User = require('./users/model')
+
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+
+server.post('/api/users', (req, res)=>{
+    const { name, bio } = req.body
+    User.insert({ name, bio })
+        .then(user => {
+            // res.status(201).json(user)
+            if(name === '' || bio === ''){
+                res.status(400).json({ message: "Please provide name and bio for the user" })
+            }else{
+                res.status(201).json(user) 
+            }
+            
+        })
+        .catch(err => {
+            res.status(500).json({ message: "There was an error while saving the user to the database" })
+        })
+})
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message })
+        })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    User.findById(id)
+        .then(user => {
+            if(!user){
+                res.status(404).json({ message: 'The user with the specified ID does not exist' })
+            }else{
+                res.status(200).json(user)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'The user information could not be retrieved' })
+        })
+})
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params
+    const { name, bio } = req.body
+    User.update(id, { name, bio })
+        .then(updatedUser => {
+            // if(!updatedUser){
+            if(name === '' || bio === ''){
+                res.status(400).json({ message: 'provide name and bio'}) 
+
+            }else{
+                if(!updatedUser){
+                    res.status(404).json({ message: 'does not exist'})
+                }else{
+                    res.json(updatedUser)   
+                }
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message })
+        })
+})
+
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params
+    User.remove(id)
+        .then(deleted => {
+            if(!deleted){
+                res.status(404).json({ message: 'does not exist'})
+            }else{
+                res.json(deleted)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message })
+        })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+
 const server = require('./api/server');
 
 const port = 5000;
 
 // START YOUR SERVER HERE
+
+server.listen(port, () => {
+    console.log('listening on port 5000')
+  })
